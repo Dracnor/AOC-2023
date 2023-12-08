76411c80d10f87b6cea75fb54482c88e1daad5bb
Dracnor <antoine.domenech@ens-lyon.fr>
2023-12-09 00:02:15 +0100

    cleaned up day1. Still no ocamllex :'( :'(

--- day1/day1.ml
+++ day1/day1.ml
@@ -4,21 +4,23 @@ let input_file = "input.txt"
 
 (* part1 *)
 (* c'est HYPER lourd, je dois passer à côté de qqchose. *)
-(* Franchement j'aurais dûd égainer ocamllex... quitte à faire du parsing... *)
+(* Franchement j'aurais dû dégainer ocamllex... quitte à faire du parsing... *)
+
+let replace_None x y =
+    if Option.is_none x then Some y else x
+
+let get_int str i =
+    (** Translates str.[i] into an int. 
+        Returns -1 if invalid char. *) 
+    if '0' <= str.[i] && str.[i] <= '9' then Char.(code str.[i] - code '0') else -1
 
 let compute_line str =
     (** Computes the integer of a line *)
-    let maj_fst fst fst_new =
-        if Option.is_none fst then Some fst_new else fst
-    in
-    let get_int i = 
-        if '0' <= str.[i] && str.[i] <= '9' then Char.(code str.[i] - code '0') else -1
-    in
     let rec loop fst lst i = 
         if i >= String.length str then Option.(get fst * 10 + get lst)
         else 
-            let n = get_int i in
-            if n <> -1 then loop (maj_fst fst n) (Some n) (i+1)
+            let n = get_int str i in
+            if n <> -1 then loop (replace_None fst n) (Some n) (i+1)
             else loop fst lst (i+1)
     in
     loop None None 0
@@ -36,27 +38,26 @@ let () =
 (* part2 *)
 let numbers = [0,"zero"; 1,"one"; 2,"two"; 3,"three"; 4,"four"; 5,"five"; 6,"six"; 7,"seven"; 8,"eight"; 9,"nine"]
 
+let try_prefix str i = 
+    (** Returns Some n if the digit n is spelled out starting at str.[i].
+        Returns None is no such n exists *)
+    try List.find_opt (fun (x, x_str) -> String.(sub str i (length str -i) |> starts_with ~prefix:x_str)) numbers
+    with Invalid_argument _ -> None (* invalid_arg can happen in String.sub near the end of str *)
+
+let get_int str i =
+    (** Translates if str.[i] into an int, including the case where it's spelled out. 
+        Returns -1 if invalid. *)
+    if '0' <= str.[i] && str.[i] <= '9' then Char.(code str.[i] - code '0') 
+    else match try_prefix str i with
+        | None -> -1
+        | Some (x,_) -> x
+
 let compute_line str =
-    let maj_fst fst fst_new =
-        if Option.is_none fst then Some fst_new else fst
-    in
-    let try_prefix i = 
-        (** Checks if a written number starts at index i *)
-        try List.find_opt (fun (x, x_str) -> String.(sub str i (length str -i) |> starts_with ~prefix:x_str)) numbers
-        with Invalid_argument _ -> None
-    in
-    let get_int i =
-        (** Each index either is an int, either starts a written number, either is nothing *)
-        if '0' <= str.[i] && str.[i] <= '9' then Char.(code str.[i] - code '0') else 
-        match try_prefix i with
-            | None -> -1
-            | Some (x,_) -> x
-    in
     let rec loop fst lst i = 
         if i >= String.length str then Option.(get fst * 10 + get lst)
         else 
-            let n = get_int i in
-            if n <> -1 then loop (maj_fst fst n) (Some n) (i+1)
+            let n = get_int str i in
+            if n <> -1 then loop (replace_None fst n) (Some n) (i+1)
             else loop fst lst (i+1)
     in
     loop None None 0
